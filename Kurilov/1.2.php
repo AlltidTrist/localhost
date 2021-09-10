db90fbacf6516348b252c19169b2acd6c1e712bb
student <321>
2021-09-10 12:32:43 +0300

    1

---
+++ Kurilov/1.2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta http-equiv="content-type" content="text/html" charset="UTF-8"/>
+    <title>Создание PHP-скриптов</title>
+</head>
+<body>
+<?php
+$dat=date('d.m.Y');
+//Вычисляем текущее время
+$tm=date('h:i:s');
+//Вычисляем текущее время
+echo 'Текущая дата:'.$dat.'<hr/>';
+echo 'Текущая дата:'.$tm.'<hr/>';
+?>
+</body>
+</html>
