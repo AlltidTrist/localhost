3c9132fe7947d305e63a2fb860ba60bee957e651
student <daniil.kurilov.2002@mail.ru>
2021-10-22 13:50:47 +0300

    1

--- Kurilov/lab6/lab6.php
+++ Kurilov/lab6/lab6.php
@@ -1,65 +1,8 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Заявка</title>
-    <link rel="stylesheet" type="text/css" href="style.css"/>
-</head>
-<body>
-<div class="Zag">
-    <h1>Форма заявки на работу в зоопарке</h1>
-    <i>Пожалуйста заполните форму. Обязательные поля помечены<span class="zvezda">*</span></i>
-</div>
-<div class="forma">
-    <fieldset>
-        <legend>Контактная информация</legend>
-        <div class="contact"><span class="nametext">Имя <span class="zvezda">*</span></span> <input type="text"
-                                                                                                    class="stroka">
-        </div>
-        <div class="contact"><span class="phonetext">Телефон</span><input type="text" class="stroka"></div>
-        <div class="contact"><span class="emailtext">Email <span class="zvezda">*</span></span> <input type="text"
-                                                                                                       class="stroka">
-        </div>
-    </fieldset>
-</div>
-<div class="forma">
-    <fieldset>
-        <legend>Персональная информация</legend>
-        <div class="forma">
-            <div class="contact"><span class="age">Возраст <span class="zvezda">*</span></span> <input type="text"
-                                                                                                       class="stroka">
-            </div>
-            <div class="contact"><span class="sex">Пол </span>
-                <select name="select">
-                    <option>Женский</option>
-                    <option>Мужской</option>
-                </select>
-            </div>
-            <div class="contact">
-                <span class="cach">Перечислите личные качества</span><textarea></textarea>
-            </div>
-        </div>
-    </fieldset>
-</div>
-
-<div class="forma">
-    <fieldset>
-        <legend>Выберите ваших любимых животных</legend>
-        <div class="zhiv">
-            <div class="chek"><input type="checkbox" class="animal"><span>Зебра</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span>Слон</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span>Кошка</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span>Антилопа</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span class="">Собака</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span>Попугай</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span>Лев</span></div>
-            <div class="chek"><input type="checkbox" class="animal"><span>Кролик</span></div>
-        </div>
-    </fieldset>
-    <input type="button" value="Отправить информацию" class="button">
-</div>
-</body>
-</html>
 <?php
-$a = fopen("my_file.html","w");
+$text = fopen('laba6text.txt','w');
+$text1 = file_get_contents('lab6.php');
+if (fwrite($text,$text1))
+    echo "Запись прошла успешно";
+else
+    echo "Произошла ошибка при записи данных.";
 ?>
